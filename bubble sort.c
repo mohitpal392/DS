9aa371479147f7d59f8f909c97988a51be8da6da
mohitpal392 <mohit.pal.cse.2025@miet.ac.in>
2026-09-25 21:12:45 +0530

    Add files via upload

---
+++ bubble sort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {5, 3, 8, 4, 2};
+    int n = 5;
+    int i, j, temp;
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted array: ");
+
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
